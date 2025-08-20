fec18aae29281194bdc846d2b8d4acb367dc5a07
Alina Zubko <ali1304sha@gmail.com>
2025-08-20 12:59:17 +0300

    added models and examples of data storage

---
+++ models/product.go
@@ -0,0 +1,14 @@
+package models
+
+type Product struct { // структура товара
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Quantity    int     `json:"quantity"`
+}
+
+type CartItem struct { //структура для товара в корзине
+	ProductID int `json:"product_id"`
+	Quantity  int `json:"quantity"`
+}
